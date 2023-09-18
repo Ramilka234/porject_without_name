609d8deec277826f2ff0d157858879ce8ca0b8e3
ramil mukhtarov <ramilasus@gmail.com>
2023-09-18 19:07:00 +0300

    СОЗДАНИЕ БДБ НАСТРОЙКА АДМИН-ПАНЕЛИ

--- WithoutName/App/admin.py
+++ WithoutName/App/admin.py
@@ -1,3 +1,94 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.contrib import admin
+from django import forms
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Category, Review, Game
+
+class GameAdminForm(forms.ModelForm):
+    """Форма с виджетом ckeditor"""
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Game
+        fields = '__all__'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ("name", )
+    list_display_links = ("name",)
+
+
+class ReviewInline(admin.TabularInline):
+    """Отзывы на странице фильма"""
+    model = Review
+    extra = 1
+    readonly_fields = ("name", "email")
+
+
+
+@admin.register(Game)
+class MovieAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ("title", "cat_id", "draft")
+    list_filter = ("cat_id", "year")
+    search_fields = ("title", "cat_id__name")
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ("draft",)
+    actions = ["publish", "unpublish"]
+    form = GameAdminForm
+    readonly_fields = ("get_image",)
+    fieldsets = (
+        (None, {
+            "fields": (("title"),)
+        }),
+        (None, {
+            "fields": ("description", ("picture", "get_image"))
+        }),
+        (None, {
+            "fields": (("year"),)
+        }),
+        ("Options", {
+            "fields": (("draft"),)
+        }),
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.picture.url} width="100" height="110"')
+
+    def unpublish(self, request, queryset):
+        """Снять с публикации"""
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+        self.message_user(request, f"{message_bit}")
+
+    def publish(self, request, queryset):
+        """Опубликовать"""
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+        self.message_user(request, f"{message_bit}")
+
+    publish.short_description = "Опубликовать"
+    publish.allowed_permissions = ('change', )
+
+    unpublish.short_description = "Снять с публикации"
+    unpublish.allowed_permissions = ('change',)
+
+    get_image.short_description = "Постер"
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы к фильму"""
+    list_display = ("name", "email", "parent", "game", "id")
+    readonly_fields = ("name", "email")
--- WithoutName/App/models.py
+++ WithoutName/App/models.py
@@ -1,2 +1,40 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField('Категория', max_length=255,)
+    description = models.TextField('Описание')
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField('Заголовок')
+    content = models.TextField('Описание')
+    picture = models.ImageField('Обложка', upload_to='games/')
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2019)
+    country = models.CharField("Страна", max_length=30, default=None, null=True, blank=True)
+    cat_id = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_review(self):
+        return self.reviews_set.filter(parent__isnull=True)
+class Review(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    parent = models.ForeignKey(
+        'self', verbose_name="Родитель", on_delete=models.SET_NULL, blank=True, null=True, related_name='children'
+    )
+    game = models.ForeignKey(Game, verbose_name='Название Игры', on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return f"{self.name} - {self.game}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+

f2acf217d4ff705d5f3c8ff5a7ab8b1a75518a86
ramil mukhtarov <ramilasus@gmail.com>
2023-09-23 00:05:35 +0300

    /

--- WithoutName/WithoutName/urls.py
+++ WithoutName/WithoutName/urls.py
@@ -21,8 +21,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api/v1/', include('App.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 if settings.DEBUG:
